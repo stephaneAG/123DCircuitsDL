e149f60bcbb1be757c0e339a5b902f849f3eb335
Stéphane Adam Garnier <seedsodesign@gmail.com>
2016-04-01 17:29:13 +0200

    Update downloadPopup.js

--- downloadPopup.js
+++ downloadPopup.js
@@ -1,5 +1,10 @@
 // get resources from the website to be used as icons :)
-var typeIcons = [ 'schematic', 'pcb', 'bom'];
+//var typeIcons = [ 'schematic', 'pcb', 'bom'];
+
+// get the circuit/project name ( 'll be used to name the files inside the zip  / DL-ed separately )
+// Nb: the || is to be able to get stuff out when either in edit mode ( logged in ) or view mode
+// R: logic is circuitTitle.substr(0, circuitTitle.indexOf('|')-3) for the altTitle
+var circuitTitle = document.querySelector('.js-circuit-menu-title').textContent || document.querySelector('.vertical-spacing--title').textContent.substr(0, document.querySelector('.vertical-spacing--title').textContent.indexOf('|')-3);
 // get SVG corresponding to typeIcons
 var menus = document.querySelectorAll('div.sitemenu__right__item--editor')
 var links = menus[1].querySelectorAll('a');
@@ -12,13 +17,34 @@ var viewsLinks = []; // holds 'em all
   viewsLinks.push([
     link.href.substr(1), // the name/type of the view
     link.querySelector('svg'), // the icon for the type of the view
-    document.querySelector(link.href + 'Tab').querySelector('svg')]) // the SVG for the view
+    document.querySelector(link.href + 'Tab').querySelector('svg'), // the SVG for the view
+    'svg' // the extension for the future file blob
+  ]);
 });
 
 // seeing the above ,why not build an array with also the views SVGs ? ( as SVGs or as blobs ? -> as SVG in a 1st time 
 )
 // now we could create <li>'s with checkbox, icon, link using items in 'viewsLinks' array
 // after doing so, we just have to add 2 more <li>'s, for the BOM ( .csv & .html versions ) ..
+// HAHA: let's add those as well to the viewLinks array ;p
+var bomLink = document.querySelector('a[href="#bom"]');
+
+var htmlTable = document.querySelector('table.bom__table');
+viewsLinks.push(
+  [
+    bomLink.substr(1), // the name/type of the view
+    bomLink.querySelector('svg'), // the icon for the type of the view
+    '<h3>' + circuitTitle + '</h3>' + htmlTable, // the content for the view
+    'html' // the extension for the future file blob
+  ],
+  [
+    bomLink.substr(1), // the name/type of the view
+    bomLink.querySelector('svg'), // the icon for the type of the view
+    circuitTitle + ' - Bill Of Materials\n' + htmlTable.innerText.split(''\t').join('','), // the content for the view
+    'csv' // the extension for the future file blob
+  ],
+);
+
 // .. and a last one for the "download zip" button ;D
 
 // clicking the links 'll 'saveAs' a blob from the corresponding stuff, except for:
@@ -40,7 +66,7 @@ var formData = document.querySelector('.js-bom-download-csv-form-data');
 var htmlTable = document.querySelector('table.bom__table');
 formData.value = JSON.stringify( htmlTable.textContent.split('\t').join(',') );
 // Nb2: WHATEVER ! -> Isn't a .csv something like the following ? ( no need for a POST for that .. )
-htmlTable.innerText.split("\t").join(",")
+'{project_name}- Bill Of Materials\n' + htmlTable.innerText.split("\t").join(",")
 // - the .zip download where we blob everything we can & zip that before proceedingto 'saveAs' user prompt
 
 // get the name of the circuit
