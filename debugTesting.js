e1d4eed4176552d703f75c320802430b2aab5057
Stéphane Adam Garnier <seedsodesign@gmail.com>
2016-04-07 20:31:26 +0200

    Create debugTesting.js

---
+++ debugTesting.js
@@ -0,0 +1,23 @@
+// when the click handlers are messed up, we test the code interactively ;p
+checkToZip('schematic_svg')
+VM2351:174 viewTypes to be zipped: schematic_svg
+undefined
+checkToZip('pcb_svg')
+VM2351:174 viewTypes to be zipped: schematic_svg,pcb_svg
+undefined
+checkToZip('bom_html')
+VM2351:174 viewTypes to be zipped: schematic_svg,pcb_svg,bom_html
+undefined
+uncheckToZip('bom_html')
+VM2351:185 viewTypes to be zipped: schematic_svg,pcb_svg
+undefined
+checkToZip('bom_html')
+VM2351:174 viewTypes to be zipped: schematic_svg,pcb_svg,bom_html
+undefined
+checkToZip('bom_csv')
+VM2351:174 viewTypes to be zipped: schematic_svg,pcb_svg,bom_html,bom_csv
+undefined
+uncheckToZip('bom_html')
+VM2351:185 viewTypes to be zipped: schematic_svg,pcb_svg,bom_csv
+undefined
+getViewFromType(toZip[2])
